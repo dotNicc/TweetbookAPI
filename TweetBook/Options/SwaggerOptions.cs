8a4a0b8ba538a12c0ab8edfbc74027e678fe246a
FrozenOne <n.courtemanche@gmail.com>
2021-02-27 12:38:38 -0500

    Tutorial 1: créer un API REST .Net 5.0 de base et implémenter Swagger

---
+++ TweetBook/Options/SwaggerOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace TweetBook.Options
+{
+    public class SwaggerOptions
+    {
+        public string JsonRoute { get; set; }
+        public string ApiDescription { get; set; }
+        public string UiEndpoint { get; set; }
+    }
+}
